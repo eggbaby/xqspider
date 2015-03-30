af084c7a0e15c91d3c3d13d1eb33bbc276bc703c
Eggb4by <eggb4by@163.com>
2015-03-30 22:31:29 +0800

    finished yet

--- fang.py
+++ fang.py
@@ -2,9 +2,7 @@
 import requests,MySQLdb,re
 from bs4 import BeautifulSoup
 
-BASE_URLS =[
-    "http://shanghai.anjuke.com/community/W0QQpZ"
-]
+BASE_URL = "http://shanghai.anjuke.com/community/W0QQpZ"
 
 
 HOST = '127.0.0.1'
@@ -12,22 +10,15 @@ USER = 'root'
 PASSWD = '811107'
 DB = 'hfhfs'
 PORT = 3306
-#xiaoqu_count,xiaoqu_add_count
-xqc = 0
-xqac = 0
-#xiaoqumain_count,xiaoqumain_add_count
-xqmc = 0
-xqmad = 0
+#total_count,add_count
+add_count = 0
+total_count = 0
+diff_count = 0
 
 conn = MySQLdb.connect(HOST,USER,PASSWD,DB,PORT)
 conn.set_character_set('utf8')
 
-cur = conn.cursor()
-cur.execute('SET NAMES utf8')
-cur.execute('SET CHARACTER SET utf8')
-cur.execute('set character_set_connection=utf8')
-
-def get_detail ( url , address):
+def get_detail ( url , address ):
 	result = []
 	detailReq = requests.get(url)
 	detailSoup = BeautifulSoup(detailReq.text)
@@ -35,47 +26,62 @@ def get_detail ( url , address):
 	xqDetail = detailSoup.find("div","comm-list").findAll("dd")
 	i = 0
 	result.append(url)
-	result.append(re.findall(r'view/\w+',url)[0])
+	result.append(re.findall(r'view/(\w+)',url)[0])
 	result.append(address)
 	for a in xqDetail :
 		# 0:name 1:district&area 5:btype 8:totalcount 9:buildtime
 		if i in (0,1,5,8,9) :
 			result.append(a.get_text())
 		i = i + 1
-	#print  re.sub(r'\s+',' ',result[3]).strip(' ').strip('\n').split(" ")
 	arealist = re.sub(r'\s+',' ',result[4]).strip(' ').strip('\n').split(" ")
+	for area in arealist :
+		result.append(area)
 	del result[4]
 	n = 0
-	for r in result :
-		result[n] = r.strip('\n')
-		n = n +1
-	for r in result :
-		print r
-		#elif i == 
-		#print a.get_text()
+	# result: 0:url 1:id 2:address 3:name 4:btype 5:totalcount 6:buildtime 7:district 8:area(optional)
+	#for r in result :
+	#	print r
+	return result
 
-for url in BASE_URLS :
-	listReq = requests.get(url)
+#add data to xiaoqu
+def add_data ( result ):
+	global total_count , add_count , diff_count
+	cur = conn.cursor()
+	cur.execute('SET NAMES utf8')
+	cur.execute('SET CHARACTER SET utf8')
+	cur.execute('set character_set_connection=utf8')
+
+	print len(result)
+	try:
+		n = cur.execute('select * from xiaoqu where id = %s'%result[1])
+		total_count = total_count + 1
+		#not found xiaoqu data
+		if n == 0 :
+			add_count = add_count + 1
+			if len(result) == 9 :
+				sql = "insert into xiaoqu(id,name,url,py,ajkid,district,area,address,buildtime,btype,totalcount,create_date) values (%s,'%s','%s','%s',%s,'%s','%s','%s','%s','%s','%s',now())"%(result[1],result[3],result[0],'',result[1],result[7],result[8],result[2],result[6],result[4],result[5])
+			else :
+				sql = "insert into xiaoqu(id,name,url,py,ajkid,district,area,address,buildtime,btype,totalcount,create_date) values (%s,'%s','%s','%s',%s,'%s','%s','%s','%s','%s','%s',now())"%(result[1],result[3],result[0],'',result[1],result[7],'',result[2],result[6],result[4],result[5])
+			
+
+			print sql
+			cur.execute(sql)
+			conn.commit()
+		else :
+			print "find data %s:%s"%(result[3],result[0])
+	except Exception,e :
+		print str(e)
+		conn.rollback()
+	#finally :
+	#	conn.close()
+
+for a in range (1,1976) :
+	listReq = requests.get(BASE_URL + str(a))
 	listSoup = BeautifulSoup(listReq.text)
-	#xqlist = listSoup.findAll(id = re.compile("comm_name_qt_apf_id\d*"))
 	xqlist = listSoup.findAll("div",{ "class":"details" })
 	for xq in xqlist :
-		#name = xq.find(id = re.compile("comm_name_qt_apf_id\d*")).get_text()
 		url = xq.find(id = re.compile("comm_name_qt_apf_id\d*")).get('href')
 		address = xq.find("div",{ "class":"t_b"}).next_sibling.next_sibling.get_text()
-		get_detail ( url,address )
-		#try:
-		#	n = cur.execute('select * from xiaoqu where url =  \'%s\''%url)
-		#	#not found xiaoqu data
-		#	if n == 0 :
-		#		xqac = xqac + 1
-		#		sql = "insert into xiaoqu(name,url,create_date) values ('%s','%s',now())"%(name,url)
-		#		#print sql
-		#		cur.execute(sql)
-		#		conn.commit()
-		#	else :
-		#		print "find data %s:%s"%(name,url)
-		#except Exception,e:
-		#	print str(e)
-		#	conn.rollback()
-print "-------------find xiaoqu : %s" % xqc + "       add xiaoqu: %s -------------" % xqac
+		result = get_detail ( url,address )
+		add_data( result )	
+print "-------------find xiaoqu : %s" % total_count + "       add xiaoqu: %s -------------" % add_count
